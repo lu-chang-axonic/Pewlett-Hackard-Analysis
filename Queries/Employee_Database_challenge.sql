7e9d2352e20d4b53c827136e00a2ac6528f84f43
Lu Chang <lchang@axoniccap.com>
2021-01-24 14:30:59 -0500

    final push

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -62,18 +62,19 @@ SELECT *
 FROM dept_emp;
 
 -- Retrieve Data
-SELECT e.emp_no,
+SELECT DISTINCT ON (e.emp_no)
+		e.emp_no,
 		e.first_name,
 		e.last_name,
 		e.birth_date,
 		de.from_date,
 		de.to_date,
 		t.title
-INTO emtorship_eligibility
+INTO mentorship_eligibility
 FROM employees as e
 INNER JOIN dept_emp as de
 ON e.emp_no=de.emp_no
 INNER JOIN titles as t
 ON e.emp_no=t.emp_no
-WHERE (de.to_date BETWEEN '1965-01-01' AND '1965-12-31')
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
